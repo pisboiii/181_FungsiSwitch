0bb4f7ab444c3281d13441b3d082520d3bd92734
pisboiii <hfzfahrzi@gmail.com>
2024-03-25 20:22:10 -0700

    menambahkan prosedur luas

--- 181_FungsiSwitch/181_FungsiSwitch.cpp
+++ 181_FungsiSwitch/181_FungsiSwitch.cpp
@@ -2,19 +2,35 @@
 //
 
 #include <iostream>
+using namespace std;
+
+int luas;
+
+void prosedurluas(int p, int l)
+{
+    luas = p * l;
+}
+
+int fungsiluas(int p, int l)
+{
+    return p = l;
+}
+
+
 
 int main()
 {
-    std::cout << "Hello World!\n";
+        int panjang, lebar;
+        cout << "masukkan Panjang: ";
+        cin >> panjang;
+        cout << "masukkan lebar :";
+        cin >> lebar;
+        
+        prosedurluas(luas, lebar);
+            cout << "luas persegi panjang" << luas << endl;
+         
+        cout << "luas persegi panjang dengan fungsi: " << fungsiluas(panjang, lebar) << luas << endl;
+
+        return 0;
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
