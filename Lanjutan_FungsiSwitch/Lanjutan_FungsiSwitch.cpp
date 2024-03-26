1e07653b9b53c7390dbba034c9ddaeec65c7b068
pisboiii <hfzfahrzi@gmail.com>
2024-03-25 20:40:12 -0700

    mebuat prosedur int penjumlahan

---
+++ Lanjutan_FungsiSwitch/Lanjutan_FungsiSwitch.cpp
@@ -0,0 +1,59 @@
+// Lanjutan_FungsiSwitch.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+int bilangan1, bilangan2;
+
+int penjumlahan(int a, int b)
+{
+    return a + b;
+}
+
+int pengurangab(int a, int b)
+{
+    return a - b;
+}
+
+int perkalian(int a, int b)
+{
+    return a * b;
+}
+
+float pembagian(float a, float b)
+{
+    return a / b;
+}
+
+void inputdata()
+{
+    cout << "|nmassukan bilangan pertama: ";
+    cin >> bilangan1;
+    cout << "masukkan bilangan kedua: ";
+    cin >> bilangan2;
+}
+
+int main()
+{
+    int pilihan;
+    do
+    {
+        system("CLS");
+    }
+    
+            
+}
+
+
+
+
+
+
+
+
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
+
